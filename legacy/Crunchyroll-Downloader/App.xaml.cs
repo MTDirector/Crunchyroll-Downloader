af5fac029dfa595ce08ddf37a9c81ad216396f92
Pierre Lagorsse <pierre.lagorsse@outlook.com>
2018-04-21 20:58:28 +0200

    Moved actual solution into legacy folder, this code will remain unchanged untile the new solution is functionnal

---
+++ legacy/Crunchyroll-Downloader/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace CrunchyrollDownloader
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
